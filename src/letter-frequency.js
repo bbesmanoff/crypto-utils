d7ea98b5977a6101566243e8168d4e5812722f4b
Brian Besmanoff <bbesmanoff@gmail.com>
2015-02-02 14:26:04 -0500

    LetterFrequency#countLetters

--- src/letter-frequency.js
+++ src/letter-frequency.js
@@ -1,5 +1,23 @@
 'use strict';
 
+var ASCII_A = 65;
+
 module.exports = {
-  countLetters: function() { }
+  countLetters: function(input) {
+    var result = {};
+
+    for (var i = 0; i < 26; i++) {
+      var key = String.fromCharCode(ASCII_A + i);
+
+      result[key] = 0;
+    }
+
+    for (var i = 0; i < input.length; i++) {
+      var ch = input[i];
+
+      result[ch]++;
+    }
+
+    return result;
+  }
 };
